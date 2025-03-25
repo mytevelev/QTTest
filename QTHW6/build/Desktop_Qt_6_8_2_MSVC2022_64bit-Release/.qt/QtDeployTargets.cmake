928e0dcd2a29e822937cb0ed3062dbb312efa270
Maxim Tevelev <tevelevmaxim@gmail.com>
2025-03-25 12:52:55 +0700

    11

--- QTHW6/build/Desktop_Qt_6_8_2_MSVC2022_64bit-Release/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Concurrent_FILE C:/Users/tevel/OneDrive/coding/Reposit/QTTest/QTHW6/build/Desktop_Qt_6_8_2_MSVC2022_64bit-Release/Concurrent.exe)
-set(__QT_DEPLOY_TARGET_Concurrent_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Concurrent_RUNTIME_DLLS C:/Users/tevel/OneDrive/coding/qt/6.8.2/msvc2022_64/bin/Qt6Widgets.dll;C:/Users/tevel/OneDrive/coding/qt/6.8.2/msvc2022_64/bin/Qt6Concurrent.dll;C:/Users/tevel/OneDrive/coding/qt/6.8.2/msvc2022_64/bin/Qt6Gui.dll;C:/Users/tevel/OneDrive/coding/qt/6.8.2/msvc2022_64/bin/Qt6Core.dll)
